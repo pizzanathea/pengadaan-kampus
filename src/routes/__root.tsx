3da1aaad3fad3b6acc20a126b498bda6e79f49ba
Affan Al Riziq <affanalriziq123@gmail.com>
2026-08-11 11:35:26 +0700

    second commit

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -104,7 +104,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/logo-ipti.png", type: "image/png" },
     ],
   }),
 
