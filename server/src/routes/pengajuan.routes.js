de4198b8e7cc5c886c1030403396eaa7ab9d1600
Raditya Bintang Wijaya <radityabintang2202@gmail.com>
2026-08-20 10:09:45 +0700

    fixing conflict

--- server/src/routes/pengajuan.routes.js
+++ server/src/routes/pengajuan.routes.js
@@ -1,26 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-// Pastikan 'uploadFiles' ikut di-import di sini dari file controller
-<<<<<<< HEAD
 const {
   getAllPengajuan,
+  getLaporan,
   createPengajuan,
   updatePengajuan,
-  uploadFiles
-=======
-const { 
-  getAllPengajuan, 
-  getLaporan,
-  createPengajuan, 
-  updatePengajuan, 
-  uploadFiles 
->>>>>>> e5ffc7c (laporan)
-} = require('../controllers/pengajuan.controller');
+  uploadFiles,
+} = require("../controllers/pengajuan.controller");
 
-router.get('/', getAllPengajuan);
-router.get('/laporan', getLaporan);
-router.post('/', uploadFiles, createPengajuan); // Pastikan uploadFiles ada di sini
-router.put('/:id', uploadFiles, updatePengajuan);
+router.get("/", getAllPengajuan);
+router.get("/laporan", getLaporan);
+router.post("/", uploadFiles, createPengajuan);
+router.put("/:id", uploadFiles, updatePengajuan);
 
 module.exports = router;
